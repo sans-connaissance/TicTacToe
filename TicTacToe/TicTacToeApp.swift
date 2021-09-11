a345c5997c706a46d0393d423c4b3f2d8d9986a6
sans-connaissance <davemalicke@gmail.com>
2021-09-10 20:33:38 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by David Malicke on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
