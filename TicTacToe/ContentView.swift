0f03afdc7d4111a61213ac10e10b50390be3199c
sans-connaissance <davemalicke@gmail.com>
2021-09-13 10:02:53 -0400

    Added animations

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -17,7 +17,9 @@ struct ContentView: View {
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(),spacing: 15), count: 3), spacing: 15, content: {
                 ForEach(0..<9) { index in
                     ZStack {
+                        Color.blue
                         Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
                         Text(moves[index])
                             .font(.system(size: 55))
                             .fontWeight(.heavy)
@@ -25,9 +27,19 @@ struct ContentView: View {
                     }
                     .frame(width: width, height: width)
                     .cornerRadius(15)
+                    .rotation3DEffect(.init(degrees: moves[index] != "" ? 180 : 0),
+                                      axis: (x: 0.0, y: 1.0, z: 0),
+                                      anchor: .center,
+                                      anchorZ: 0.0,
+                                      perspective: 1.0)
                     .onTapGesture {
-                        moves[index] = currentUser ? "x" : "o"
-                        self.currentUser.toggle()
+                        withAnimation(Animation.easeIn(duration: 0.3)) {
+                            if moves[index] == "" {
+                                moves[index] = currentUser ? "x" : "o"
+                                self.currentUser.toggle()
+                            }
+                        }
+                        
                     }
                 }
             }).padding()
