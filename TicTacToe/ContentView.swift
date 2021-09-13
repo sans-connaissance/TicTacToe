417587695c3322282ea28eda2c2fc1d9ec4d89a3
sans-connaissance <davemalicke@gmail.com>
2021-09-13 09:50:12 -0400

    Added onTapGesture functions

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var moves: [String] = Array(repeating: "", count: 9)
+    @State var currentUser = false
+    
     var body: some View {
         NavigationView {
             LazyVGrid(columns: Array(repeating: GridItem(.flexible(),spacing: 15), count: 3), spacing: 15, content: {
                 ForEach(0..<9) { index in
-                    Color.white
-                        .frame(width: width, height: width)
-                        .cornerRadius(15)
+                    ZStack {
+                        Color.white
+                        Text(moves[index])
+                            .font(.system(size: 55))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: width, height: width)
+                    .cornerRadius(15)
+                    .onTapGesture {
+                        moves[index] = currentUser ? "x" : "o"
+                        self.currentUser.toggle()
+                    }
                 }
             }).padding()
             .preferredColorScheme(.dark)
